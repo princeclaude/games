b6b2960accfddc52641c3f0ccb18722a205a5995
princeclaude9528 <techmadeus@gmail.com>
2025-09-05 21:25:36 +0100

    toast upgraded

--- src/hooks/useInviteSocket.js
+++ src/hooks/useInviteSocket.js
@@ -1,13 +1,14 @@
 import { useEffect } from "react";
 import { socket } from "../utils/socket";
-import { toast } from "react-toastify";
+import { useToast } from "../context/ToastContext";
 
 export const useInviteSocket = (fetchInvitations) => {
+  const { addToast } = useToast();
   useEffect(() => {
     socket.on("new-invite", (invite) => {
       console.log("📩 New invite received:", invite);
-      fetchInvitations(); // refresh badge immediately
-      toast.info(`${invite.from} invited you to play ${invite.gameName}`);
+      fetchInvitations(); 
+      addToast(`${invite.from} invited you to play ${invite.gameName}`, "info");
     });
 
     return () => {
