0293764f51e7418529ebe328b3695878f090b352
princeclaude9528 <techmadeus@gmail.com>
2025-09-05 17:21:16 +0100

    invitation counter fix

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -5,7 +5,6 @@ import { FaHome, FaBell,FaCompass, FaCog } from "react-icons/fa";
 const BottomNav = ({ invitationCount = 0 }) => {
   return (
     <nav className="fixed bottom-0 left-0 w-full bg-gray-900 border-t border-gray-700 flex justify-around items-center py-2 z-50">
-      
       <NavLink
         to="/dashboard"
         className={({ isActive }) =>
@@ -16,8 +15,8 @@ const BottomNav = ({ invitationCount = 0 }) => {
       >
         <FaHome className="text-lg" />
         <span>Home</span>
-          </NavLink>
-          
+      </NavLink>
+
       <NavLink
         to="/explore"
         className={({ isActive }) =>
@@ -30,24 +29,25 @@ const BottomNav = ({ invitationCount = 0 }) => {
         <span>Explore</span>
       </NavLink>
 
-      
       <NavLink
         to="/Expect"
         className={({ isActive }) =>
-          `relative flex flex-col items-center text-sm ${
+          `flex flex-col items-center text-sm ${
             isActive ? "text-purple-400" : "text-gray-400"
           }`
         }
       >
-        <FaBell className="text-lg" />
+        <div className="relative">
+          <FaBell className="text-lg" />
+          {invitationCount > 0 && (
+            <span className="absolute -top-1 -right-2 bg-red-500 text-white text-xs font-bold rounded-full px-1">
+              {invitationCount}
+            </span>
+          )}
+        </div>
         <span>Invitations</span>
-        {invitationCount > 0 && (
-          <span className="absolute -top-1 right-3 bg-red-500 text-white text-xs font-bold rounded-full px-1">
-            {invitationCount}
-          </span>
-        )}
       </NavLink>
-
+      
       {/* Settings */}
       <NavLink
         to="/settings"
